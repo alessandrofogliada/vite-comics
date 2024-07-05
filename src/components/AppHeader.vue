cf7bce2a882fa6f6ef76c9fa695072b92686be37
alessandrofogliada <fogliada.a@gmail.com>
2024-07-05 16:24:45 +0200

    1.0.0 Creazione struttura base

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <header>
+        <div>
+            <img src="/public/img/dc-logo.png" alt="">
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+// aggiugniamo scoped perchè vogliamo che le modifiche di css che attuiamo saranno esclusivamente per questo parte , ovvero Header 
+
+</style>
